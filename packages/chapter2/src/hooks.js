4fc6e22762cd95ce144260f47aa52d2ef372663e
이재협 <zzok3312@naver.com>
2024-03-22 23:29:31 +0900

    feat:과제 B. useState, useMemo 만들기

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,44 @@
 export function createHooks(callback) {
+  const states = [];
+  let statesKey = 0;
   const useState = (initState) => {
-    return [];
+    let key = statesKey;
+    const state = states[key] !== undefined ? states[key] : initState;
+
+    const setState = (newVal) => {
+      if (states[key] === newVal) return;
+      states[key] = newVal;
+      callback();
+    };
+
+    statesKey++;
+
+    return [state, setState];
   };
 
-  const useMemo = (fn, refs) => {
-    return fn();
+  const useMemo = (fn, deps) => {
+    const isEqual = (arr1, arr2) => {
+      if (arr1.length !== arr2.length) {
+        return false;
+      }
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    if (!states[statesKey] || !isEqual(deps, states[statesKey][1])) {
+      states[statesKey] = [fn(), deps];
+    }
+
+    return states[statesKey][0];
   };
 
   const resetContext = () => {
-
-  }
+    statesKey = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
