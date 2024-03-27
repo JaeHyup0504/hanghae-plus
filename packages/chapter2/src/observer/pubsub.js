4b35d1eabb60a5c6333a9c77899ab33195baf6a6
이재협 <zzok3312@naver.com>
2024-03-28 00:37:07 +0900

    feat:과제 E. 구독/발행 패턴 구현

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,26 @@
-export const 구독 = fn => {
-  fn();
-}
+let currentCallback;
 
-export const 발행기관 = obj => {
+export const 구독 = (fn) => {
+  currentCallback = fn;
+  fn();
+};
+
+export const 발행기관 = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    let value = obj[key];
+    let sub = new Set();
+
+    Object.defineProperty(obj, key, {
+      get() {
+        sub.add(currentCallback);
+        return value;
+      },
+      set(newValue) {
+        value = newValue;
+        sub.forEach((fn) => fn());
+      },
+    });
+  });
+
   return obj;
-}
+};
